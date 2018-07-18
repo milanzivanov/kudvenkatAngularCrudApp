ee54177eddaafafffb2e5437bd5732ee1d6bfeb8
milan.zivanov <milan.zivanov@gmail.com>
2018-07-18 13:23:49 +0200

    checkbox in angular

--- src/app/employess/create-employee.component.ts
+++ src/app/employess/create-employee.component.ts
@@ -8,7 +8,8 @@ import { NgForm } from '@angular/forms';
 })
 export class CreateEmployeeComponent implements OnInit {
 
-  gender = 'male';
+  // isActive = true;
+  // gender = 'male';
 
   constructor() { }
 
